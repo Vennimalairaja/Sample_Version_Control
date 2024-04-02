327ca3bd47629fb1a9ac3d3cf1e9f43c60394b88
vennnimalairaja <kvennimalairaja@gmail.com>
2024-04-02 22:55:21 +0530

    Added an uswr input

--- new.py
+++ new.py
@@ -1,4 +1,10 @@
+
 def hello():
     print("Hi how are you !")
+    a=input("What's your name ?")
+    if a=="Raja":
+	print("Hello Raja nice to meet you")
+    else:
+	print("OOMBU da punda!")
 if __name__=="__main__":
    hello()
